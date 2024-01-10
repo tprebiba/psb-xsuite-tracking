cfbf9fb1e72523bd57d9554ed10c6cc32bc7033d
tprebiba <tirsi.prebibaj@cern.ch>
2024-01-10 16:48:57 +0100

    updated htcondor files

--- htcondor_executable.sh
+++ htcondor_executable.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "uname -r:" `uname -r`
 
-#export MYPYTHON=/afs/cern.ch/user/t/tprebiba/python_environments/base-2022/miniconda
 source /usr/local/xsuite/miniforge3/bin/activate xsuite
 pwd
 
-current_directory="/home/tprebiba/eos/Fellowship/03_Xsuite/psb-xsuite-tracking"
+current_directory=/home/tprebiba/eos/Fellowship/03_Xsuite/psb-xsuite-tracking
 
 cp -r $current_directory/psb .
 cp -r $current_directory/input .
@@ -15,12 +14,8 @@ cp -r $current_directory/time_tables .
 cp $current_directory/*.py .
 cp $current_directory/*.sh .
 
-#source $MYPYTHON/bin/activate ""
-#export PATH=$MYPYTHON/bin:$PATH
 echo "which python:" `which python`
 
 date
-#DIR=/afs/cern.ch/work/t/tprebiba/half-integer_xsuite/000_PSB_half-integer_dynamic_crossing_PIC_reference
-#python $DIR/runPSB.py
 python runPSB.py
 date
